520b9130ed36ace889ef6755c5d8dab2b0ea1985
Lembani Sakala <lembanisakala@gmail.com>
2022-05-13 06:41:59 +0200

    Update app entry point - invoke the getScores methos - add eventListeners for the reset button & submit button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import './style.css';
-import showScores from './modules/show-scores.js';
+import getScores from './modules/get-scores.js';
+import addScore from './modules/add-score.js';
 
-document.addEventListener('DOMContentLoaded', showScores);
+const resetBtn = document.querySelector('.reset-btn');
+const submitBtn = document.querySelector('.submit-btn');
+
+getScores();
+
+resetBtn.addEventListener('click', () => {
+  getScores();
+});
+
+submitBtn.addEventListener('click', () => {
+  addScore();
+});
